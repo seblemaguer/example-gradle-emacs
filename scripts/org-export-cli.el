19cdccf2c6bccaa6069fbc6aaae00921f7525394
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-05-29 15:17:32 +0200

    Starting cli/gradle integration

---
+++ scripts/org-export-cli.el
@@ -0,0 +1,206 @@
+(unless (>= emacs-major-version 25)
+  (error "Error: emacs version 25 or greater is required"))
+
+(setq lexical-binding t)
+(provide 'cli)
+
+(defun cli-do-nothing () t)
+
+(defun cli-option-p (str)
+  "Return t if str is not nil and starts with '--'"
+  (and str (string-equal (substring str 0 2) "--")))
+
+(defun cli-opt-name (str)
+  "Return the option name (strips leading '--')"
+  (substring str 2 nil))
+
+(defun cli-required-p (optdef)
+  "Return t if the option defined in `optdef` (an element of
+`options-alist') is required."
+  (eq (length optdef) 2))
+
+(defun cli-parse-args (options-alist &optional docstring arguments)
+  "Parses a list of arguments according to the specifiction
+provided by `options-alist' and returns a hashmap of option names
+to values. Arguments are read from `command-line-args' unless an
+optional list `arguments' is provided.
+
+`options-alist' is of the form
+
+'((required-option help-text)
+  (option help-text default-value)
+  (boolean-option help_text nil))
+
+Each option must be a string beginning with '--'. If
+default-value is not provided, the option is required and an
+error will be raised if it is missing.
+
+Arguments are of the form '--option <value>'. If 'value' is
+missing and a default value is defined, 'option' will be given a
+value of t (this is useful for defining boolean command line
+parameters).
+
+Command line arguments already defined by emacs (see 'emacs
+--help') are reserved and cannot be used.
+
+Note that this function has a side effect: arbitrary command line
+arguments are allowed by assigning `command-line-functions` a
+value of `cli-do-nothing'.
+"
+
+  (setq command-line-functions '(cli-do-nothing))
+  (let ((clargs (or arguments command-line-args))
+        (args (make-hash-table :test 'equal))
+        (opt nil)
+        (optname nil)
+        (optiondef nil)
+        (val nil)
+        (hashval nil))
+
+    ;; print help text and exit if command line contains -h or -h-org
+    (if (member "-h" clargs)
+        (progn (cli-show-help options-alist docstring)
+               (kill-emacs 0)))
+
+    ;; set defaults
+    (mapc #'(lambda (optdef)
+              (if (eq (length optdef) 3)
+                  (puthash (cli-opt-name (car optdef)) (nth 2 optdef) args))
+              ) options-alist)
+
+    ;; set options from command line arguments
+    (while clargs
+      (setq opt (car clargs))
+      (setq optname (if (cli-option-p opt) (cli-opt-name opt)))
+      (if optname
+          (progn
+            (setq optiondef (assoc opt options-alist))
+            (if optiondef
+                ;; If val is provided, add it to args. Otherwise,
+                ;; store a value of t unless the option is required.
+                (progn
+                  (unless (cli-option-p (nth 1 clargs)) (setq val (nth 1 clargs)))
+                  (if (not (cli-required-p optiondef)) (setq val (or val t)))
+                  (puthash optname val args))
+              (error (format "Error: the option '%s' is not defined" opt)))
+            ))
+      (setq clargs (cdr clargs)))
+
+    ;; check for required arguments
+    (mapc #'(lambda (optdef)
+              (setq hashval (gethash (cli-opt-name (car optdef)) args))
+              (if (and (eq (length optdef) 2) (not hashval))
+                  (error (format "Error: a value for the option '%s' is required"
+                                 (car optdef))))
+              ) options-alist)
+
+    args
+    ))
+
+(defun cli-show-help (options-alist &optional docstring)
+  "Display options, defaults and help text defined in
+`options-alist' (see `cli-parse-args' for specification)"
+  (let ((max-width
+         (apply 'max (mapcar #'(lambda (opt) (length (car opt))) options-alist)))
+        (fstr nil))
+
+    (princ "Command line options:\n\n")
+    (setq fstr (format " %%-%ss  %%s\n" max-width))
+    (mapc #'(lambda (opt)
+              (princ (format fstr (nth 0 opt) (nth 1 opt))))
+          options-alist)
+    (princ "\n")
+    (if docstring (princ docstring))
+    (princ "\n")))
+
+;; package management
+(defun cli-package-installed-p (package &optional min-version)
+  "Return true if PACKAGE, of MIN-VERSION or newer, is installed
+(ignoring built-in versions).  MIN-VERSION should be a version list"
+
+  (unless package--initialized (error "package.el is not yet initialized!"))
+
+(if (and (< emacs-major-version 24) (< emacs-minor-version 4))
+    ;; < emacs 24.4
+    (let ((pkg-desc (assq package package-alist)))
+      (if pkg-desc
+          (version-list-<= min-version
+                           (package-desc-vers (cdr pkg-desc)))))
+  ;; >= emacs 24.4
+  (let ((pkg-descs (cdr (assq package package-alist))))
+    (and pkg-descs
+         (version-list-<= min-version
+                          (package-desc-version (car pkg-descs)))))
+  ))
+
+(defun cli-install-packages (package-list)
+  "Install each package named in PACKAGE-LIST using elpa if not
+already installed."
+  (mapc #'(lambda (pkg)
+            (unless (cli-package-installed-p pkg)
+              (message "installing %s..." pkg)
+              (unless package-archive-contents
+                (package-refresh-contents))
+              ;; (package-menu-refresh)
+              (package-install pkg)
+              (message "done installing %s" pkg))
+            ) package-list))
+
+(defvar cli-default-package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("melpa" . "http://melpa.org/packages/")
+    ("org" . "http://orgmode.org/elpa/")))
+
+(defun cli-package-setup (emacs-directory package-list &optional upgrade archives)
+  (unless (file-readable-p emacs-directory)
+    (message (format "Creating directory %s" emacs-directory))
+    (make-directory emacs-directory t))
+
+  ;; must assign `user-emacs-directory' *before* requiring `package'
+  (setq user-emacs-directory emacs-directory)
+
+  ;; also seems to be necessary to define `package-archives' before
+  ;; requiring `package'
+  (setq package-archives (or archives cli-default-package-archives))
+
+  (require 'package)
+  (package-initialize)
+
+  (if upgrade
+      (progn
+        (package-list-packages)
+        (package-menu-mark-upgrades)
+        (package-menu-execute))
+    (package-list-packages-no-fetch))
+
+  (cli-install-packages package-list))
+
+;; other utilities
+
+(defun cli-replace-all (from-str to-str)
+  "Replace all occurrences of from-str with to-str in current buffer"
+  (beginning-of-buffer)
+  (while (search-forward from-str nil t)
+    (replace-match to-str nil t)))
+
+;; only executed if this is the script called from the command line
+;; (ie, "if __name__ == '__main__'")
+(if (member (file-name-nondirectory load-file-name)
+            (mapcar 'file-name-nondirectory command-line-args))
+    (progn
+      (setq options-alist
+            '(("--package-dir" "directory containing elpa packages" "~/.org-export")
+              ("--package-upgrade" "Perform package upgrade" nil)
+              ))
+
+      (defvar docstring "
+Manage elpa packages
+
+")
+
+      (setq args (cli-parse-args options-alist docstring))
+      (defun getopt (name) (gethash name args))
+
+      (cli-package-setup
+       (getopt "package-dir") '(org htmlize) (getopt "package-upgrade"))
+      ))
---
+++ scripts/org-export-reveal.el
@@ -0,0 +1,227 @@
+(require 'cli (concat (file-name-directory load-file-name) "org-export-cli.el"))
+
+;; (byte-compile-file (concat (file-name-directory load-file-name) "cli.el"))
+(setq options-alist
+      '(("--infile" "path to input .org file")
+        ("--outfile" "path to output .html file (use base name of infile by default)"
+         nil)
+        ("--evaluate" "evaluate source code blocks by default" nil)
+        ("--css" "path or URL of css" nil)
+        ("--embed-css" "Include contents of css in a <style> block" nil)
+        ("--bootstrap" "make Bootstrap-specific modifications to html output;
+                        if selected, link to Bootstrap CDN by default" nil)
+        ("--package-dir" "directory containing elpa packages" "~/.org-export")
+        ("--verbose" "enable debugging message on error" nil)
+        ))
+
+(setq args (cli-parse-args options-alist "
+Options --infile and --outfile are required. Note that code block
+evaluation is disabled by default; use '--evaluate' to set a
+default value of ':eval yes' for all code blocks. If you would
+like to evaluate by default without requiring this option,
+include '#+PROPERTY: header-args :eval yes' in the file
+header. Individual blocks can be selectively evaluated using
+':eval yes' in the block header.
+"))
+
+(defun getopt (name) (gethash name args))
+
+
+(cli-package-setup (getopt "package-dir") '(ess htmlize org org-plus-contrib ox-reveal org-ref))
+(require 'ox)
+(require 'ox-reveal)
+(require 'org-ref)
+;; (require 'ox-bibtex)
+(require 'ox-extra)
+(ox-extras-activate '(ignore-headlines))
+
+(setq debug-on-error (getopt "verbose"))
+
+;; general configuration
+(setq make-backup-files nil)
+
+;; ess configuration
+(add-hook 'ess-mode-hook
+          '(lambda ()
+             (setq ess-ask-for-ess-directory nil)
+             ))
+
+;; css configuration
+(defvar bootstrap-url
+  "http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css")
+
+(defvar css-url (getopt "css"))
+(if (getopt "bootstrap")
+    (setq css-url (or css-url bootstrap-url)))
+
+(defvar my-html-head "")
+(if css-url
+    (if (getopt "embed-css")
+        ;; embed css contents in a <style> block
+        (progn
+          (setq my-html-head
+                (format "<style type=\"text/css\">\n%s\n</style>\n"
+                        (if (string-match "^http" css-url)
+                            ;; use the contents of file at path
+                            (with-current-buffer
+                                (url-retrieve-synchronously css-url)
+                              (message (format "Inserting contents of %s" css-url))
+                              (buffer-string))
+                          ;; use the contents of the file at css-url
+                          (with-temp-buffer
+                            (insert-file-contents css-url)
+                            (buffer-string)))
+                        )))
+      ;; ...or add a link to the css file
+      (setq my-html-head
+            (format
+             "<link rel=\"stylesheet\" type=\"text/css\" href=\"%s\" />" css-url))))
+
+;; org-mode and export configuration
+
+;; store the execution path for the current environment and provide it
+;; to sh code blocks - otherwise, some system directories are
+;; prepended in the code block's environment. Would be nice to figure
+;; out where these are coming from. This solves the problem for shell
+;; code blocks, but not for other languages (like python).
+(defvar exec-path-str
+  (mapconcat 'identity exec-path ":"))
+
+(defvar sh-src-prologue
+  (format "export PATH=\"%s\"" exec-path-str))
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (setq org-confirm-babel-evaluate nil)
+             (setq org-export-allow-BIND 1)
+             (setq org-html-doctype "html5")
+             (setq org-html-head my-html-head)
+             (setq org-babel-sh-command "bash")
+             (setq org-babel-default-header-args
+                   (list `(:session . "none")
+                         `(:eval . ,(if (getopt "evaluate") "yes" "no"))
+                         `(:results . "output replace")
+                         `(:exports . "both")
+                         `(:cache . "no")
+                         `(:noweb . "no")
+                         `(:hlines . "no")
+                         `(:tangle . "no")
+                         `(:padnewline . "yes")
+                         ))
+
+             ;; explicitly set the PATH in sh code blocks; note that
+             ;; `list`, the backtick, and the comma are required to
+             ;; dereference sh-src-prologue as a variable; see
+             ;; http://stackoverflow.com/questions/24188100
+             (setq org-babel-default-header-args:sh
+                   (list `(:prologue . ,sh-src-prologue)))
+
+             (org-babel-do-load-languages
+              (quote org-babel-load-languages)
+              (quote ((R . t)
+                      (latex . t)
+                      (python . t)
+                      (shell . t)
+                      (sql . t)
+                      (sqlite . t)
+                      (emacs-lisp . t)
+                      (dot . t)
+                      )))
+             ))
+
+
+(defun endless/export-audio-link (path desc format)
+  "Export org audio links to hmtl."
+  (cl-case format
+    (html (format "<audio src=\"%s\" controls>%s</audio>" path (or desc "")))
+    (latex (format "(HOW DO I EXPORT AUDIO TO LATEX? \"%s\")" path))))
+
+(org-add-link-type "audio" #'ignore #'endless/export-audio-link)
+
+
+(defun org-reveal-export-as-html
+  (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to an HTML buffer.
+
+If narrowing is active in the current buffer, only export its
+narrowed part.
+
+If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
+
+When optional argument SUBTREEP is non-nil, export the sub-tree
+at point, extracting information from the headline properties
+first.
+
+When optional argument VISIBLE-ONLY is non-nil, don't export
+contents of hidden elements.
+
+When optional argument BODY-ONLY is non-nil, only write code
+between \"<body>\" and \"</body>\" tags.
+
+EXT-PLIST, when provided, is a property list with external
+parameters overriding Org default settings, but still inferior to
+file-local settings.
+
+Export is done in a buffer named \"*Org REVEAL Export*\", which
+will be displayed when `org-export-show-temporary-export-buffer'
+is non-nil."
+  (interactive)
+  (org-export-to-buffer 'reveal "*Org REVEAL Export*"
+    async subtreep visible-only body-only ext-plist
+    (lambda () (set-auto-mode t))))
+
+
+;; Redefined to avoid problem with sizing
+(defun org-html--svg-image (source attributes info)
+  "Return \"object\" embedding svg file SOURCE with given ATTRIBUTES.
+INFO is a plist used as a communication channel.
+
+The special attribute \"fallback\" can be used to specify a
+fallback image file to use if the object embedding is not
+supported.  CSS class \"org-svg\" is assigned as the class of the
+object unless a different class is specified with an attribute."
+  (org-html-close-tag
+     "img"
+     (org-html--make-attribute-string
+      (org-combine-plists
+       (list :src source
+	     :alt (if (string-match-p "^ltxpng/" source)
+		      (org-html-encode-plain-text
+		       (org-find-text-property-in-string 'org-latex-src source))
+		    (file-name-nondirectory source)))
+       attributes))
+     info))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;; compile and export ;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar infile (getopt "infile"))
+(defvar outfile
+  (file-truename
+   (or (getopt "outfile")
+       (replace-regexp-in-string "\.org$" ".html" infile))))
+
+;; remember the current directory; find-file changes it
+(defvar cwd default-directory)
+
+;; copy the source file to a temporary file; note that using the
+;; infile as the base name defines the working directory as the same
+;; as the input file
+(defvar infile-temp (make-temp-name (format "%s.temp." infile)))
+(copy-file infile infile-temp t)
+(find-file infile-temp)
+
+(org-mode)
+(org-reveal-export-as-html)
+
+(write-file outfile)
+
+;; clean up
+(setq default-directory cwd)
+(delete-file infile-temp)
